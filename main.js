4e9b590ba7de75ea91e615312ac26b60a76c7df2
Adithya Kalhara <adithyakalhara12345@gmail.com>
2025-07-18 09:33:08 +0530

    add recources

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".text",{
+    strings: ["Front-end Developer","Back-end Developer","Full-stack Developer","Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
